454a7564b1c666f1da838a8bd9796560a1ceeeea
Quaxdrone(Nijo Fukushi) <137780492+Quaxdrone@users.noreply.github.com>
2023-08-01 18:42:37 +0300

    Add files via upload

---
+++ framework.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+#include <d3d11.h>
+#pragma comment(lib, "d3d11.lib")
